1644bf9a72fe23c294248d04af423c3f7c24d8c9
Dev26112001 <devjyotimohanty17@gmail.com>
2024-06-07 15:09:33 +0530

    second commit

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,20 +1,14 @@
 "use client";
 
-import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  // const { data: accounts, isLoading } = useGetAccounts();
-
-  // if (isLoading) {
-  //   return <div>Loading...</div>;
-  // }
-
-  // return (
-  //   <div>
-  //     {accounts?.map((account) => (
-  //       <div key={account.id}>{account.name}</div>
-  //     ))}
-  //   </div>
-  // );
-  return <div>Dashboard</div>;
+  const { onOpen } = useNewAccount();
+  return (
+    <div>
+      <Button onClick={onOpen}>New Account</Button>
+    </div>
+  );
 }
